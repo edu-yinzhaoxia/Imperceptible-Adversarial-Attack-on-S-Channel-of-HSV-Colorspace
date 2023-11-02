4c15cf9055d750db40be9935b8a5ba095fc0d836
edu-yinzhaoxia <71623447+edu-yinzhaoxia@users.noreply.github.com>
2023-11-02 11:21:31 +0800

    Add files via upload

---
+++ Original_HSV_Pert.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+
+from torchattacks.attack import Attack
+from differential_color_functions import rgb2lab_diff, ciede2000_diff
+
+
+class RGB_HSV(nn.Module):
+
+    def __init__(self, eps=1e-8):
+        super(RGB_HSV, self).__init__()
+        self.eps = eps
+
+    def rgb_to_hsv(self, img):
+        hue = torch.Tensor(img.shape[0], img.shape[2], img.shape[3]).to(img.device)
+
+        hue[img[:, 2] == img.max(1)[0]] = 4.0 + ((img[:, 0] - img[:, 1]) / (img.max(1)[0] - img.min(1)[0] + self.eps))[
+            img[:, 2] == img.max(1)[0]]
+        hue[img[:, 1] == img.max(1)[0]] = 2.0 + ((img[:, 2] - img[:, 0]) / (img.max(1)[0] - img.min(1)[0] + self.eps))[
+            img[:, 1] == img.max(1)[0]]
+        hue[img[:, 0] == img.max(1)[0]] = (0.0 + ((img[:, 1] - img[:, 2]) / (img.max(1)[0] - img.min(1)[0] + self.eps))[
+            img[:, 0] == img.max(1)[0]]) % 6
+
+        hue[img.min(1)[0] == img.max(1)[0]] = 0.0
+        hue = hue / 6
+
+        saturation = (img.max(1)[0] - img.min(1)[0]) / (img.max(1)[0] + self.eps)
+        saturation[img.max(1)[0] == 0] = 0
+
+        value = img.max(1)[0]
+
+        hue = hue.unsqueeze(1)
+        saturation = saturation.unsqueeze(1)
+        value = value.unsqueeze(1)
+        hsv = torch.cat([hue, saturation, value], dim=1)
+        return hsv
+
+    def hsv_to_rgb(self, hsv):
+        h, s, v = hsv[:, 0, :, :], hsv[:, 1, :, :], hsv[:, 2, :, :]
+        # 对出界值的处理
+        h = h % 1
+        s = torch.clamp(s, 0, 1)
+        v = torch.clamp(v, 0, 1)
+
+        r = torch.zeros_like(h)
+        g = torch.zeros_like(h)
+        b = torch.zeros_like(h)
+
+        hi = torch.floor(h * 6)
+        f = h * 6 - hi
+        p = v * (1 - s)
+        q = v * (1 - (f * s))
+        t = v * (1 - ((1 - f) * s))
+
+        hi0 = hi == 0
+        hi1 = hi == 1
+        hi2 = hi == 2
+        hi3 = hi == 3
+        hi4 = hi == 4
+        hi5 = hi == 5
+
+        r[hi0] = v[hi0]
+        g[hi0] = t[hi0]
+        b[hi0] = p[hi0]
+
+        r[hi1] = q[hi1]
+        g[hi1] = v[hi1]
+        b[hi1] = p[hi1]
+
+        r[hi2] = p[hi2]
+        g[hi2] = v[hi2]
+        b[hi2] = t[hi2]
+
+        r[hi3] = p[hi3]
+        g[hi3] = q[hi3]
+        b[hi3] = v[hi3]
+
+        r[hi4] = t[hi4]
+        g[hi4] = p[hi4]
+        b[hi4] = v[hi4]
+
+        r[hi5] = v[hi5]
+        g[hi5] = p[hi5]
+        b[hi5] = q[hi5]
+
+        r = r.unsqueeze(1)
+        g = g.unsqueeze(1)
+        b = b.unsqueeze(1)
+        rgb = torch.cat([r, g, b], dim=1)
+        return rgb
+
+
+class SPGD(Attack):
+    def __init__(self, model, eps=0.3,
+                 alpha=2 / 255, steps=40, which_channel=1, random_start=False):
+        super().__init__("PGD", model)
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.model = model
+        self.random_start = random_start
+        self._supported_mode = ['default', 'targeted']
+        self.which_channel = which_channel
+
+    def rgb_to_hsv(self, img, eps=1e-8):
+        hue = torch.Tensor(img.shape[0], img.shape[2], img.shape[3]).to(img.device)
+
+        hue[img[:, 2] == img.max(1)[0]] = 4.0 + ((img[:, 0] - img[:, 1]) / (img.max(1)[0] - img.min(1)[0] + eps))[
+            img[:, 2] == img.max(1)[0]]
+        hue[img[:, 1] == img.max(1)[0]] = 2.0 + ((img[:, 2] - img[:, 0]) / (img.max(1)[0] - img.min(1)[0] + eps))[
+            img[:, 1] == img.max(1)[0]]
+        hue[img[:, 0] == img.max(1)[0]] = (0.0 + ((img[:, 1] - img[:, 2]) / (img.max(1)[0] - img.min(1)[0] + eps))[
+            img[:, 0] == img.max(1)[0]]) % 6
+
+        hue[img.min(1)[0] == img.max(1)[0]] = 0.0
+        hue = hue / 6
+
+        saturation = (img.max(1)[0] - img.min(1)[0]) / (img.max(1)[0] + eps)
+        saturation[img.max(1)[0] == 0] = 0
+
+        value = img.max(1)[0]
+
+        hue = hue.unsqueeze(1)
+        saturation = saturation.unsqueeze(1)
+        value = value.unsqueeze(1)
+        hsv = torch.cat([hue, saturation, value], dim=1)
+        return hsv
+
+    def hsv_to_rgb(self, hsv):
+        h, s, v = hsv[:, 0, :, :], hsv[:, 1, :, :], hsv[:, 2, :, :]
+        # 对出界值的处理
+        h = h % 1
+        s = torch.clamp(s, 0, 1)
+        v = torch.clamp(v, 0, 1)
+
+        r = torch.zeros_like(h)
+        g = torch.zeros_like(h)
+        b = torch.zeros_like(h)
+
+        hi = torch.floor(h * 6)
+        f = h * 6 - hi
+        p = v * (1 - s)
+        q = v * (1 - (f * s))
+        t = v * (1 - ((1 - f) * s))
+
+        hi0 = hi == 0
+        hi1 = hi == 1
+        hi2 = hi == 2
+        hi3 = hi == 3
+        hi4 = hi == 4
+        hi5 = hi == 5
+
+        r[hi0] = v[hi0]
+        g[hi0] = t[hi0]
+        b[hi0] = p[hi0]
+
+        r[hi1] = q[hi1]
+        g[hi1] = v[hi1]
+        b[hi1] = p[hi1]
+
+        r[hi2] = p[hi2]
+        g[hi2] = v[hi2]
+        b[hi2] = t[hi2]
+
+        r[hi3] = p[hi3]
+        g[hi3] = q[hi3]
+        b[hi3] = v[hi3]
+
+        r[hi4] = t[hi4]
+        g[hi4] = p[hi4]
+        b[hi4] = v[hi4]
+
+        r[hi5] = v[hi5]
+        g[hi5] = p[hi5]
+        b[hi5] = q[hi5]
+
+        r = r.unsqueeze(1)
+        g = g.unsqueeze(1)
+        b = b.unsqueeze(1)
+        rgb = torch.cat([r, g, b], dim=1)
+        return rgb
+
+    def forward(self, images, labels):
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+
+        loss = nn.CrossEntropyLoss()
+
+        adv_images = images.clone().detach()
+
+        if self.random_start:
+            # Starting at a uniformly random point
+            adv_images = self.rgb_to_hsv(adv_images)
+            adv_images[:, self.which_channel, :, :] += torch.empty_like(adv_images[:, self.which_channel, :, :])\
+                                                            .uniform_(-self.eps, self.eps)
+            adv_images = self.hsv_to_rgb(adv_images)
+
+        for num in range(self.steps):
+            hsv_image = self.rgb_to_hsv(adv_images)
+            hsv_image.requires_grad = True
+            reconstructed_image = self.hsv_to_rgb(hsv_image)
+
+            # d_map=ciede2000_diff(rgb2lab_diff(images,self.device),rgb2lab_diff(reconstructed_image,self.device),self.device).unsqueeze(1)
+            # color_dis = torch.norm(d_map.view(1, -1), dim=1)
+            # color_loss = color_dis.sum()
+
+            outputs = self.model(reconstructed_image)
+
+            cost = loss(outputs, labels)
+
+            self.model.zero_grad()
+            cost.backward()
+
+            perturbated_hsv = hsv_image.clone().detach()
+            perturbated_hsv[:, self.which_channel, :, :] += self.alpha * hsv_image.grad.sign()[:, self.which_channel, :, :]
+
+            perturbated_image = self.hsv_to_rgb(perturbated_hsv)
+            delta = torch.clamp(perturbated_image - images, min=-self.eps, max=self.eps)
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+
+            if self.model(adv_images).argmax(dim=1)[0] != labels[0]:
+                break
+
+
+        return adv_images, num+1
